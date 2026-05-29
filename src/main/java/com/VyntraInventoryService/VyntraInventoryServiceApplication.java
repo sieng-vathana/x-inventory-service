711cd7b26ef312d283899f7c49a09fe4eceb6a5f
Vathana Programmer <vathana@example.com>
2026-05-29 19:17:32 +0700

    Initial commit

---
+++ src/main/java/com/VyntraInventoryService/VyntraInventoryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.VyntraInventoryService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VyntraInventoryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VyntraInventoryServiceApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/VyntraInventoryService/controller/HomeController.java
@@ -0,0 +1,16 @@
+package com.VyntraInventoryService.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/home")
+public class HomeController {
+
+    @GetMapping("/{message}")
+    public String getMessage(@PathVariable String message) {
+        return "Welcome to Vyntra Home Service message: " + message ;
+    }
+}
