27c786a5764b0140225a44d3aa866d08a63f3659
Sieng Vathana <siengvathana1@gmail.com>
2026-08-22 15:01:02 +0700

    Add movement history filtering support

--- src/main/java/com/x/inventory/repository/InventoryMovementRepository.java
+++ src/main/java/com/x/inventory/repository/InventoryMovementRepository.java
@@ -2,6 +2,8 @@ package com.x.inventory.repository;
 
 import com.x.inventory.entity.InventoryMovement;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
-public interface InventoryMovementRepository extends JpaRepository<InventoryMovement, Long> {
+public interface InventoryMovementRepository
+        extends JpaRepository<InventoryMovement, Long>, JpaSpecificationExecutor<InventoryMovement> {
 }
